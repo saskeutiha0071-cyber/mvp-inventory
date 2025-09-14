fa97fb131bd593292eec9a7e7500e10ce5ab51b2
saskeutiha0071-cyber <saskeutiha0071@gmail.com>
2025-09-14 15:49:48 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: false, // Отключи source maps для продакшена
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   }
 })
